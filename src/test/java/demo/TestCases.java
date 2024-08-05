9513c381a7dcf2ae29fabb2674e21bf1d925aea2
Vishnukrishnan M <vishkrishman@gmail.com>
2024-08-05 22:40:03 +0530

    ME_QA_XGOOGLE_FORM_MODULE_L1

--- src/test/java/demo/TestCases.java
+++ src/test/java/demo/TestCases.java
@@ -1,21 +1,55 @@
 package demo;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.logging.LogType;
 import org.openqa.selenium.logging.LoggingPreferences;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import java.util.logging.Level;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 // import io.github.bonigarcia.wdm.WebDriverManager;
 import demo.wrappers.Wrappers;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class TestCases {
     ChromeDriver driver;
+
+    @FindBy(xpath = "//*[text()='Your answer']/preceding-sibling::input")
+    WebElement nameField;
+
+    @FindBy(tagName = "textarea")
+    WebElement reasonField;
+
+    // @FindBy(xpath = "//div[@class='Qr7Oae'][3]")
+    // WebElement divExperience;
+
+    @FindBy(xpath = "//*[@data-value='3 - 5']")
+    WebElement experienceValue;
+
+    @FindBy(xpath =  "//input[@type='date']")
+    WebElement dateField;
+
+    @FindBy(xpath = "//input[@aria-label='Hour']")
+    WebElement HourField;
+
+    @FindBy(xpath = "//input[@aria-label='Minute']")
+    WebElement MinuteField;
+
+    @FindBy(xpath = "//div[@role='button']//span[text()='Submit']")
+    WebElement SubmitButton;
+
+    @FindBy(className = "vHW8K")
+    WebElement thanksElem;
 
     /*
      * TODO: Write your tests here with testng @Test annotation. 
@@ -26,6 +60,46 @@ public class TestCases {
     /*
      * Do not change the provided methods unless necessary, they will help in automation and assessment
      */
+
+     /* hahahahah */
+    @Test
+    public void testCase01() throws InterruptedException{
+        driver.get("https://docs.google.com/forms/d/e/1FAIpQLSep9LTMntH5YqIXa5nkiPKSs283kdwitBBhXWyZdAS-e4CxBQ/viewform");
+        Wrappers.sendKeyWrap(driver,nameField,"Crio Learner");
+        String reason = "I want to be the best QA Engineer! " + System.currentTimeMillis();
+        Wrappers.sendKeyWrap(driver, reasonField, reason);
+
+        // Wrappers.scrollTo(driver, divExperience);
+        Wrappers.click(driver, experienceValue);
+
+        Wrappers.checkClick(driver, "Java");
+        Wrappers.checkClick(driver, "Selenium");
+        Wrappers.checkClick(driver, "TestNG");
+        
+        Wrappers.GenderWrapper(driver, "Ms");
+        
+        LocalDate date = LocalDate.now().minusDays(7);
+        DateTimeFormatter dateformat = DateTimeFormatter.ofPattern("ddMMYYYY");
+        // Wrappers.sendKeyWrap(driver, dateField, date.format(dateformat));
+        Wrappers.scrollTo(driver, dateField, "true");
+        dateField.sendKeys(date.format(dateformat));
+        // Wrappers.sendKeyWrap(driver, dateField, date.format(dateformat), "false");
+
+        // LocalDateTime ldt = LocalDateTime.now();
+        // DateTimeFormatter hourFormat = DateTimeFormatter.ofPattern("HH");
+        // DateTimeFormatter minuteformat = DateTimeFormatter.ofPattern("mm");
+        
+        // Wrappers.sendKeyWrap(driver, HourField, ldt.format(hourFormat));
+        // Wrappers.sendKeyWrap(driver, MinuteField, ldt.format(minuteformat));
+        Wrappers.sendKeyWrap(driver, HourField, "07");
+        Wrappers.sendKeyWrap(driver, MinuteField, "30");
+
+        Wrappers.click(driver, SubmitButton);
+
+        System.out.println(thanksElem.getText());
+        Thread.sleep(5000);
+    }
+
     @BeforeTest
     public void startBrowser()
     {
@@ -47,6 +121,7 @@ public class TestCases {
         driver = new ChromeDriver(options);
 
         driver.manage().window().maximize();
+        PageFactory.initElements(driver,this);
     }
 
     @AfterTest
--- src/test/java/demo/wrappers/Wrappers.java
+++ src/test/java/demo/wrappers/Wrappers.java
@@ -1,19 +1,60 @@
 package demo.wrappers;
 
+import java.util.*;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
-
 public class Wrappers {
     /*
      * Write your selenium wrappers here
      */
+    public static void sendKeyWrap(WebDriver driver, WebElement element, String text){
+        scrollTo(driver, element, "true");
+        element.click();
+        element.sendKeys(text);
+    }
+    public static void sendKeyWrap(WebDriver driver, WebElement element, String text, String flag){
+        scrollTo(driver, element, flag);
+        element.click();
+        element.sendKeys(text);
+    }
+
+    public static void click(WebDriver driver, WebElement element){
+        scrollTo(driver, element, "true");
+        element.click();
+    }
+
+    public static void checkClick(WebDriver driver, String text){
+        WebElement element = driver.findElement(By.xpath("//*[text()='" + text + "']"));
+        scrollTo(driver, element, "true");
+        element.click();
+    }
+
+    public static void GenderWrapper(WebDriver driver, String choice) throws InterruptedException{
+        WebElement selectionBox = driver.findElement(By.xpath("//*[text()='Choose']"));
+        scrollTo(driver, selectionBox, "true");
+        selectionBox.click();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofMillis(3000));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@role='option']//span[@class='vRMGwf oJeWuf'][normalize-space()='"+choice+"']")));
+        
+        WebElement item = driver.findElement(By.xpath("//div[@role='option']//span[@class='vRMGwf oJeWuf'][normalize-space()='"+choice+"']"));
+        item.click();
+        Thread.sleep(500);
+     
+    }
+
+    public static void scrollTo(WebDriver driver, WebElement element,String flag){
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView("+flag+")",element);
+    }
 }
